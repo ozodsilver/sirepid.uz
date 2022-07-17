9ef2430e569ff634ac0fce177d5fda31d42d1f41
ozodsilver <ozodsilver@gmail.com>
2022-07-17 09:01:36 +0500

    counter added

--- scripts.js
+++ scripts.js
@@ -8,34 +8,35 @@ function showCoords(event) {
     let y = event.screenY;
    
 if(x > 730 && y < 590){
-    h1.style.cssText = 'transform:translate(-10px, 5px)'
-    knopka.style.cssText = 'transform:translate(-8px, 2px)'
+    h1.style.cssText = 'transform:translate(-7px, 3px)'
+    knopka.style.cssText = 'transform:translate(-5px, 2px)'
 }
 
 if(x < 570 && y < 590){
-    h1.style.cssText = 'transform:translate(20px, 5px)'
-    knopka.style.cssText = 'transform:translate(10px, 2px)'
+    h1.style.cssText = 'transform:translate(10px, 4px)'
+    knopka.style.cssText = 'transform:translate(7px, 2px)'
 }
 
 if(x < 575 && y > 716){
-    h1.style.cssText = 'transform:translate(30px, -10px)'
+    h1.style.cssText = 'transform:translate(10px, -10px)'
     knopka.style.cssText = 'transform:translate(10px, -4px)'
 }
 
 if(x > 800 && y > 700){
-    h1.style.cssText = 'transform:translate(-10px, -5px)'
-    knopka.style.cssText = 'transform:translate(7px, -10px)'
+    h1.style.cssText = 'transform:translate(-6px, -2px)'
+    knopka.style.cssText = 'transform:translate(7px, -7px)'
 }
 
   }
 
   document.addEventListener('scroll', ()=>{
     let offsetY = window.pageYOffset;
-for(let i=0; i<=20; i++){
-    if(offsetY>0){
+for(let i=0; i<=3; i++){
+    if(offsetY<40){
        
         h1.style.cssText = `margin-top:${pageYOffset}px`
     }
+   
 
     if(offsetY==0){
 setTimeout(() => {
@@ -52,3 +53,25 @@ setTimeout(() => {
     }
 }
   })
+
+
+
+  window.onload = ()=>{
+    // $(selector).countMe(delay,speed)
+    $("#num1").countMe(40,65);
+    $("#num2").countMe(30, 30);
+    $("#num3").countMe(40, 50);
+    $("#num4").countMe(30,50);
+ }
+
+
+ var _gaq = _gaq || [];
+ _gaq.push(['_setAccount', 'UA-36251023-1']);
+ _gaq.push(['_setDomainName', 'jqueryscript.net']);
+ _gaq.push(['_trackPageview']);
+
+ (function() {
+   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+ })();
